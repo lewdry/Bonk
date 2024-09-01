7cff2b1f4d6490066ebbce408131dc0489f26a4d
Lewis Dryburgh <lewdry@users.noreply.github.com>
2024-09-01 22:25:19 +1000

    Update bonk.js
    commented out listener

--- bonk.js
+++ bonk.js
@@ -133,7 +133,7 @@ function initGame() {
     resetGame();
 
     canvas.addEventListener('pointerdown', handleStart, false);
-    canvas.addEventListener('touchstart', handleStart, false);
+    // don't use this for now - canvas.addEventListener('touchstart', handleStart, false);
     canvas.addEventListener('pointermove', handleMove, false);
     canvas.addEventListener('pointerup', handleEnd, false);
     canvas.addEventListener('pointercancel', handleEnd, false);
